c4ca520a3c143ef7930d75bbfc1fb3a938cea1bd
Faizan <syedfaizan.edu@gmail.com>
2024-08-01 01:33:24 +0530

    added navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const Navbar = () => {
+  return (
+    <div className="drop-shadow-sm fixed flex top-4 space-x-5 justify-center items-center w-1/3 z-50 bg-red-700  text-white rounded-r-full rounded-l-full py-2">
+      <div>Home</div>
+      <div>Project</div>
+      <div>Contact </div>
+    </div>
+  )
+}
+
+export default Navbar
