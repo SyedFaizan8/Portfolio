c8d25eb07ae1485f1b6f22a213964769d19f8d2c
Faizan <syedfaizan.edu@gmail.com>
2024-08-06 14:36:38 +0530

    moved header and footer from layout.ts to page.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
-"use client"
-
 import type { Metadata } from 'next';
 import { Inter, Playfair_Display } from 'next/font/google';
 import './globals.css';
-import Navbar from '@/components/Navbar';
-import WavyBgFooter from '@/components/WavyBgFooter';
-import { useEffect } from 'react';
-import Lenis from 'lenis'
 
 const inter = Inter({ subsets: ['latin'] });
 const play = Playfair_Display({ subsets: ['latin'], variable: "--font_play" });
@@ -22,35 +16,10 @@ interface RootLayoutProps {
 
 export default async function RootLayout({ children }: RootLayoutProps) {
 
-  useEffect(() => {
-    //scroll
-    const lenis = new Lenis()
-    function raf(time: any) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-    requestAnimationFrame(raf)
-
-    //contextmenu
-    const handleContextMenu = (e: MouseEvent) => {
-      e.preventDefault();
-    };
-    document.addEventListener('contextmenu', handleContextMenu);
-    return () => {
-      document.removeEventListener('contextmenu', handleContextMenu);
-    };
-  }, [])
-
   return (
     <html lang="en" >
       <body className={`${inter.className}  ${play.variable}`}>
-        <nav>
-          <Navbar />
-        </nav>
         {children}
-        <footer>
-          <WavyBgFooter />
-        </footer>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,39 @@
 import HeroSection from "@/components/HomeSection";
 import { Profile } from "@/components/Profile";
 import Project from "@/components/Project";
+import { useEffect } from 'react';
+import Lenis from 'lenis'
+import Navbar from "@/components/Navbar";
+import WavyBgFooter from "@/components/WavyBgFooter";
 
 export default function Home(): JSX.Element {
 
+  useEffect(() => {
+    //scroll
+    const lenis = new Lenis()
+    function raf(time: any) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+
+    //contextmenu
+    const handleContextMenu = (e: MouseEvent) => {
+      e.preventDefault();
+    };
+    document.addEventListener('contextmenu', handleContextMenu);
+    return () => {
+      document.removeEventListener('contextmenu', handleContextMenu);
+    };
+  }, [])
+
   return (
     <>
+      <Navbar />
       <HeroSection id="home" />
       <Project id="project" />
       <Profile id="about" />
+      <WavyBgFooter />
     </>
   );
 }
