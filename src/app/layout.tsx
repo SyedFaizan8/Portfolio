597b898b1e7464bb71586a42bbfac795e6493014
Faizan <syedfaizan.edu@gmail.com>
2024-08-13 23:20:45 +0530

    added prevent default for the links and added icons to and metadata baseurl in the meta data of layout page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,24 @@ export const metadata: Metadata = {
   title: 'Syed Faizan',
   description: 'Explore Faizan\'s portfolio showcasing projects and skills in software development. Discover a fresh perspective and connect for exciting opportunities.',
   keywords: ["Syed", "Faizan", "Syed Faizan portfolio", "software engineer portfolio", "professional portfolio", "Software Developer", "Web Developer", "Mobile Developer", "DevOps Engineer", "Cybersecurity Specialist", "Full-stack developer", "Portfolio", "Computer Science Engineering", "Project Showcase", "Football", "Next.js Portfolio", "Developer Portfolio"],
+  icons: {
+    icon: [
+      { url: 'icon.png' },
+      { url: 'icon2.png' }
+    ],
+    apple: '/apple-icon.png'
+  },
+  metadataBase: new URL('https://syedfaizan.in'),
+  appleWebApp: {
+    title: 'Syed Faizan Portfolio',
+    statusBarStyle: 'black-translucent',
+    startupImage: [
+      {
+        url: '/apple-icon.png',
+        media: '(device-width: 768px) and (device-height: 1024px)',
+      },
+    ],
+  },
 };
 
 interface RootLayoutProps {
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import { Link } from "react-scroll"
+import React, { MouseEvent } from "react"
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+
+  const prevent = (e: MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="lg:mx-auto text-sm lg:text-lg lg:left-0 lg:right-0 lg:top-3 lg:rounded-r-full lg:rounded-l-full lg:w-2/5  xl:w-2/6 from-neutral-200 font-bold drop-shadow-lg bg-opacity-10 backdrop-blur-sm fixed flex justify-center items-center w-full py-3 z-50 bg-black ">
       <div className="space-x-5">
@@ -10,6 +16,7 @@ const Navbar = () => {
           to="home"
           smooth="easeInOutQuad"
           duration={500}
+          onClick={() => prevent}
         >
           <button>Home</button>
         </Link>
@@ -18,6 +25,7 @@ const Navbar = () => {
           smooth="easeInOutQuad"
           duration={500}
           offset={-80}
+          onClick={() => prevent}
         >
           <button>Projects</button>
         </Link>
@@ -26,6 +34,7 @@ const Navbar = () => {
           smooth="easeInOutQuad"
           duration={500}
           offset={-10}
+          onClick={() => prevent}
         >
           <button>About Me</button>
         </Link>
@@ -33,6 +42,7 @@ const Navbar = () => {
           to="contact"
           smooth="easeInOutQuad"
           duration={500}
+          onClick={() => prevent}
         >
           <button>Contact</button>
         </Link>
