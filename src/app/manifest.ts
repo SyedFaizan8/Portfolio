89e7024222bf71790de2c0fc532c8f26bed1273d
Faizan <syedfaizan.edu@gmail.com>
2024-08-19 14:39:58 +0530

    removed chrome images

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,16 +15,16 @@ export default function manifest(): MetadataRoute.Manifest {
                 sizes: '48x48',
                 type: 'image/x-icon',
             },
-            {
-                src: "/android-chrome-192x192.png",
-                sizes: "192x192",
-                type: "image/png"
-            },
-            {
-                src: "/android-chrome-512x512.png",
-                sizes: "512x512",
-                type: "image/png"
-            }
+            // {
+            //     src: "/android-chrome-192x192.png",
+            //     sizes: "192x192",
+            //     type: "image/png"
+            // },
+            // {
+            //     src: "/android-chrome-512x512.png",
+            //     sizes: "512x512",
+            //     type: "image/png"
+            // }
         ]
     }
 
